a81ce59821f709e6837bbd98e85d7d207709aa73
AlexKolykhalov <ak8647@rambler.ru>
2024-07-22 14:33:32 +0300

    Changed PORT and added vercel.json

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import path from 'path';
 
 const __dirname = path.resolve();
-const port = process.env.PORT ?? 2000;
+const port = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'static')));
 
-app.listen('2000', () => {
+app.listen(port, () => {
     console.log(`Server has been started at port: ${port}...`);
 });
