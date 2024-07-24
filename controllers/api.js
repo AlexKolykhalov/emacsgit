eda698e2607ff739ee77626627bb6d0153dc45ac
AlexKolykhalov <ak8647@rambler.ru>
2024-07-24 10:59:04 +0300

    Remove all console.log

--- controllers/api.js
+++ controllers/api.js
@@ -15,9 +15,7 @@ const get = async (req, res) => {
 const update = async (req, res) => {
     try {
 	const {id, number} = req.body;
-	console.log(`update data id: ${id} number: ${number}`);
 	const data = await db.query('UPDATE number_list SET data = $1 WHERE id = $2 RETURNING *', [number, id]);
-	console.log(data);
 	res.status(200).json(data.rows[0]);
     } catch (error) {
 	res.status(400).send('Error');
@@ -26,7 +24,6 @@ const update = async (req, res) => {
 
 const remove = async (req, res) => {
     const id = req.params.id;
-    console.log(`remove ID: ${id}`);
     if (id) {
 	await db.query('DELETE FROM number_list WHERE id = $1;', [id]);
 	res.status(204).send('Data deleted');
@@ -36,4 +33,4 @@ const remove = async (req, res) => {
 };
 
 
-export {create, get, update, remove};
+export { create, get, update, remove };
